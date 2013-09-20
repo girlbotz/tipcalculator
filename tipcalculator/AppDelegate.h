056d1d0afff740fa331e7642e6352927a2ff972a
Sharon <sharonjhau@gmail.com>
2013-09-19 22:17:59 -0700

    Initial Commit

---
+++ tipcalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  tipcalculator
+//
+//  Created by Sharon Hau on 9/19/13.
+//  Copyright (c) 2013 Sharon Hau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ tipcalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  tipcalculator
+//
+//  Created by Sharon Hau on 9/19/13.
+//  Copyright (c) 2013 Sharon Hau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

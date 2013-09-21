360d60233b6094dd53912ae018d2d1c586135062
Sharon <sharonjhau@gmail.com>
2013-09-20 23:12:20 -0700

    Checking complete tip calculator

---
+++ tipcalculator/TipViewController.h
@@ -0,0 +1,13 @@
+//
+//  TipViewController.h
+//  tipcalculator
+//
+//  Created by Sharon Hau on 9/19/13.
+//  Copyright (c) 2013 Sharon Hau. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TipViewController : UIViewController
+
+@end
